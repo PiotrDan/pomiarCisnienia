0f6c30971ed9f12641d500c6b9045fe6027e8392
Piotr <pdanilewicz@poczta.fm>
2023-02-17 19:43:59 +0100

    Create basics folders and setups

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, flash, request, redirect
+import sqlite3
+from datetime import datetime, date
+
+app = Flask(__name__)
+
+if app.config['ENV'] =='production':
+  app.config.from_object('config.ProductionConfig')
+elif app.config['ENV'] == 'development':
+  app.config.from_object('config.DevelopmentConfig')
+else:
+  app.config.from_object('config.TestingConfig')    
+
+@app.route('/')
+def index():
+  return '<h1>Cześć</h1>'
